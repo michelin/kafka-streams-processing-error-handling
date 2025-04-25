c211bec3b059762b4cf80e21d5e22c25a9246f0a
Loïc Greffier <loic.greffier@michelin.com>
2025-04-25 19:38:08 +0200

    Update processor Id and record type handlers

--- dsl/src/main/java/com/michelin/kafka/error/handling/dsl/handler/ProcessorIdProcessingHandler.java
+++ dsl/src/main/java/com/michelin/kafka/error/handling/dsl/handler/ProcessorIdProcessingHandler.java
@@ -35,6 +35,6 @@ public class ProcessorIdProcessingHandler implements ProcessingExceptionHandler 
 
     @Override
     public void configure(Map<String, ?> map) {
-
+        // Do nothing
     }
 }
--- dsl/src/main/java/com/michelin/kafka/error/handling/dsl/handler/RecordTypeProcessingHandler.java
+++ dsl/src/main/java/com/michelin/kafka/error/handling/dsl/handler/RecordTypeProcessingHandler.java
@@ -5,20 +5,33 @@ import java.util.Map;
 import org.apache.kafka.streams.errors.ErrorHandlerContext;
 import org.apache.kafka.streams.errors.ProcessingExceptionHandler;
 import org.apache.kafka.streams.processor.api.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RecordTypeProcessingHandler implements ProcessingExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(RecordTypeProcessingHandler.class);
+
     @Override
     public ProcessingHandlerResponse handle(ErrorHandlerContext context, Record<?, ?> record, Exception exception) {
-        if (record.value() instanceof DeliveryBooked deliveryBooked) {
+        log.warn(
+            "Exception caught for processorNodeId = {}, topic = {}, partition = {}, offset = {}, key = {}, value = {}",
+            context.processorNodeId(),
+            context.topic(),
+            context.partition(),
+            context.offset(),
+            record != null ? record.key() : null,
+            record != null ? record.value() : null,
+            exception);
+
+        if (record != null && record.value() instanceof DeliveryBooked deliveryBooked) {
             return deliveryBooked.getNumberOfTires() == null ? ProcessingHandlerResponse.CONTINUE : ProcessingHandlerResponse.FAIL;
         }
 
         return ProcessingHandlerResponse.CONTINUE;
-
     }
 
     @Override
     public void configure(Map<String, ?> map) {
-
+        // Do nothing
     }
 }
